facdcba78b14cfe7f86a71cdd65967187e3e89fd
wphr2101 <wphr2101@hotmail.com>
2023-01-27 23:14:47 -0500

    Update servers.component.ts

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,38 @@
 import { Component } from "@angular/core";
 
 @Component({
+  //selector: ".app-servers",
   selector: "app-servers",
   templateUrl: "./servers.component.html",
+  /* template: `<h3>Servers Component</h3>
+    <hr />
+    <div class="first"><app-server></app-server></div>
+
+    <div class="second"><app-server></app-server></div>
+
+    <div class="third"><app-server></app-server></div>`,*/
   styleUrls: ["./servers.component.css"]
+  /*styles: [
+    `
+      h3 {
+        color: red;
+        font-size: 24px;
+      }
+
+      .first {
+        font-size: 20px;
+        font-weight: bold;
+      }
+
+      .second {
+        font-size: 20px;
+        font-weight: lighter;
+      }
+
+      .third {
+        color: green;
+      }
+    `
+  ]*/
 })
 export class ServersComponent {}
